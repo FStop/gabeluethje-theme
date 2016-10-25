938b6a751105ff6577d0277919e567b6b47c07a4
Gabriel Luethje <gabriel.luethje@fusion.net>
2016-10-24 22:17:42 -0500

    Add sass-lint

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,19 @@
 'use strict';
 
-var gulp = require('gulp');
-var sass = require('gulp-sass');
-var livereload = require('gulp-livereload');
+var gulp = require('gulp'),
+    sass = require('gulp-sass'),
+    livereload = require('gulp-livereload'),
+    sassLint = require('gulp-sass-lint')
+
+gulp.task('sass-lint'), function() {
+    return gulp.src('./assets/scss/**/*.scss')
+    .pipe(sassLint())
+    .pipe(sassLint.format())
+    .pipe(sassLint.failOnError())
+}
 
 gulp.task('sass', function () {
-  return gulp.src('./assets/scss/style.scss')
+    return gulp.src('./assets/scss/style.scss')
     .pipe(sass().on('error', sass.logError))
     .pipe(gulp.dest('./assets/css'))
     .pipe(livereload());
@@ -15,3 +23,5 @@ gulp.task('watch', function () {
     livereload.listen();
     gulp.watch('./assets/scss/**/*.scss', ['sass']);
 });
+
+gulp.task('default',['sass-lint', 'sass', 'watch']);
