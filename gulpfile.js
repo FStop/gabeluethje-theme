ec38c02504bef974d374b4c91e54d2c0d6bb41be
Gabriel Luethje <gabriel.luethje@fusion.net>
2016-12-13 10:54:16 -0600

    Add browsersync to gulpfile, remove livereload

--- gulpfile.js
+++ gulpfile.js
@@ -1,44 +1,59 @@
 'use strict';
 
 var gulp = require('gulp'),
-    sass = require('gulp-sass'),
-    sourcemaps = require('gulp-sourcemaps'),
-    livereload = require('gulp-livereload'),
-    sassLint = require('gulp-sass-lint'),
-    uglify = require('gulp-uglify'),
-    concat = require('gulp-concat');
+  sass = require('gulp-sass'),
+  sourcemaps = require('gulp-sourcemaps'),
+  sassLint = require('gulp-sass-lint'),
+  uglify = require('gulp-uglify'),
+  concat = require('gulp-concat'),
+  browserSync = require('browser-sync'),
+  reload = browserSync.reload;
 
 gulp.task('sass-lint'), function() {
-    return gulp.src('./assets/scss/**/*.scss')
+  return gulp.src('./assets/scss/**/*.scss')
     .pipe(sassLint())
     .pipe(sassLint.format())
     .pipe(sassLint.failOnError())
 }
 
 gulp.task('sass', function () {
-    return gulp.src('./assets/scss/style.scss')
+  return gulp.src('./assets/scss/style.scss')
     .pipe(sourcemaps.init())
-    .pipe(sass({
-        //
-    }).on('error', sass.logError))
+    .pipe(sass({}).on('error', sass.logError))
     .pipe(sourcemaps.write('./maps'))
     .pipe(gulp.dest('./assets/css'))
-    .pipe(livereload());
+    .pipe(reload({stream:true}))
+});
+
+
+// browser-sync task for starting the server.
+gulp.task('browser-sync', function() {
+  //watch files
+  var files = [
+  './assets/css/style.css',
+  './assets/js/src/*.js',
+  './*.php'
+  ];
+
+  // initialize browsersync
+  browserSync.init(files, {
+  // browsersync with a php server
+  proxy: "actualgabe.dev",
+  notify: false
+  });
 });
 
 // Concatenate & Minify JS
 gulp.task('scripts', function() {
-    return gulp.src('./assets/js/src/*.js')
+  return gulp.src('./assets/js/src/*.js')
     .pipe(concat('scripts.min.js'))
     .pipe(uglify())
     .pipe(gulp.dest('assets/js/dist'))
-    .pipe(livereload())
 });
 
 gulp.task('watch', function () {
-    livereload.listen();
-    gulp.watch(['./assets/scss/**/*.scss', '**/*.php'], ['sass-lint', 'sass']);
-    gulp.watch('./assets/js/src/*.js', ['scripts']);
+  gulp.watch(['./assets/scss/**/*.scss', '**/*.php'], ['sass-lint', 'sass']);
+  gulp.watch('./assets/js/src/*.js', ['scripts']);
 });
 
-gulp.task('default',['sass-lint', 'sass', 'scripts', 'watch']);
+gulp.task('default',['sass-lint', 'sass', 'scripts', 'browser-sync', 'watch']);
